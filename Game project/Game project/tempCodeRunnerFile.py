0421eb5d4d5216098925b0a2eb4dd0b0172ff5f5
Adityapartap1 <76278413+Adityapartap1@users.noreply.github.com>
2024-05-25 22:40:25 +0530

    Dungeon Hunter

---
+++ Game project/Game project/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+ # def world1(self):
+      #       self.root.destroy()
+      #       pygame.time.set_timer(self.enemy_generation, 2000)
+      #       button.imgdisp = 1
+      #       self.world = 1
+      #       castle.hide = True
+      #       self.battle = True
+            
+      #       mmanager.playsoundtrack(soundtrack[1], -1, 0.05)
